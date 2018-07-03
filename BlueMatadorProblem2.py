c3e1d3fe6fa23b89dfb1ac9c34085565ff93bec0
ajmalesa <u0766202@umail.utah.edu>
2018-07-03 00:53:27 -0600

    Problem 2 done but code needs cleaning/organizing/commenting

--- BlueMatadorProblem2.py
+++ BlueMatadorProblem2.py
@@ -1,4 +1,61 @@
-import json, request
+import json, random
+from urllib.request import Request, urlopen
 
-response = request.get("https://jsonplaceholder.typicode.com/todos")
-todos = json.loads(response.text)
+import null as null
+
+chosen_category = 'scienceefaew'
+search_query = 'newtoeafafn'
+
+def jokeCategory(category):
+
+    url_of_available_categories = 'https://api.chucknorris.io/jokes/categories'
+    available_categories = Request(url_of_available_categories, headers={'User-Agent': 'Mozilla/5.0'})
+
+    non_decoded_categories = urlopen((available_categories)).read()
+
+    decoded_categories = non_decoded_categories.decode('utf-8')
+
+    deserialized_categories = json.loads(decoded_categories)
+
+    if(category not in deserialized_categories):
+        return None
+
+    url = 'https://api.chucknorris.io/jokes/random?category=' + category
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    non_decoded_page= urlopen(req).read()
+
+    decoded_page = non_decoded_page.decode('utf-8')
+
+    deserialized_page = json.loads(decoded_page)
+
+    if(len(deserialized_page) == 0):
+        return None
+
+    return deserialized_page['value']
+
+
+def jokeSearch(query):
+
+    url = 'https://api.chucknorris.io/jokes/search?query=' + query
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    non_decoded_page= urlopen(req).read()
+
+    decoded_page = non_decoded_page.decode('utf-8')
+
+    deserialized_page = json.loads(decoded_page)
+
+    all_results = deserialized_page['result']
+
+    if(len(all_results) == 0):
+        return None
+
+    random_joke_from_results = all_results[random.randrange(0, len(all_results))]
+
+    joke = random_joke_from_results['value']
+
+    return joke
+
+
+print(jokeCategory(chosen_category))
+print(jokeSearch(search_query))
